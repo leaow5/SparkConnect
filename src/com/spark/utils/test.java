297711a7d46a0c12cbd807b35d3ebad5af8da792
zxp <leaow5@gmail.com>
2016-03-28 20:33:02 +0800

    git测试

--- src/com/spark/utils/test.java
+++ src/com/spark/utils/test.java
@@ -5,12 +5,8 @@ import java.io.UnsupportedEncodingException;
 public class test {
 
 	public static void main(String[] args) throws UnsupportedEncodingException {
-		String chinaString = "你好";
+		String chinaString = "提交git测试";
 
-		byte[] s1 =HexAsciiStringConvertUtil.gbkToBytes(chinaString);
-		String s2 =HexAsciiStringConvertUtil.bytesToGbk(s1);
-
-		System.out.println(s2);
 	}
 
 }
